27fd1727b7ea0806d6994d56ce457bf0e6e3e9d7
gtictunit <gtictunit2014@gmail.com>
2022-06-12 10:00:38 +0100

    App Name Change

--- android/app/src/main/java/com/gt/mobile/streaming/app/MainActivity.java
+++ android/app/src/main/java/com/gt/mobile/streaming/app/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
     @Override
   protected String getMainComponentName() {
-    return "GT_MOBILE_STREAMING";
+    return "LifeSprings";
   }
 
   @Override
--- ios/SangeetTests/SangeetTests.m
+++ ios/SangeetTests/SangeetTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface GT_MOBILE_STREAMINGTests : XCTestCase
+@interface LifeSpringsTests : XCTestCase
 
 @end
 
-@implementation GT_MOBILE_STREAMINGTests
+@implementation LifeSpringsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
